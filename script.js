663c9ec3911366a15f30e6810fc768d7d68dc8c0
stephy <stephshiro35@gmail.com>
2024-03-17 22:12:53 +0300

    add

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+
+
+
+
+    // Get references to the image and text elements
+    const project1 = document.getElementById('project1');
+    
+
+    // Add click event listeners to the image and text elements
+    project1.addEventListener('mouseover', function (){
+        project1.style.opacity = '0.5';
+    });
+
+    project1.addEventListener('mouseout', function () {
+        project1.style.opacity = '1';
+    });
+
+    const project2 = document.getElementById('project2');
+    
+
+    // Add click event listeners to the image and text elements
+    project2.addEventListener('mouseover', function (){
+        project2.style.opacity = '0.5';
+    });
+
+    project2.addEventListener('mouseout', function () {
+        project2.style.opacity = '1';
+    });
+
+    const project3 = document.getElementById('project3');
+    
+
+    // Add click event listeners to the image and text elements
+    project3.addEventListener('mouseover', function (){
+        project3.style.opacity = '0.5';
+    });
+
+    project3.addEventListener('mouseout', function () {
+        project3.style.opacity = '1';
+    });
+
