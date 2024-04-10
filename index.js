3f33831743cb2ff6fb08922ce702bef5f3d76b14
stephy <stephshiro35@gmail.com>
2024-04-10 22:40:08 +0300

    add

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
-function addToCart() {
-    // Add your logic here to add the item to the cart
-    alert('Item added to cart!');
+fetch('./profile.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+    
+
+
+    let profile = document.getElementById("profile");
+   
+   
+
+
+    profile.innerHTML += `<div class="artist" id="artist1">
+    <img src="img/profilepic1.jpg" alt="Artist 1">
+    <!-- Content for artist 1 -->
+    <h3>WANJIRU OMONDI</h3>
+    <p>Specializes in abstract painting and oilbased paints</p>
+</div>`;
+
+for (let i = 0; i < data.length; i++) {
+
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
